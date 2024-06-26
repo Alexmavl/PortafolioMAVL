31e6df934d52f2e19fad65695ca8131ca8392599
Marvin Vásquez <114128290+Alexmavl@users.noreply.github.com>
2024-06-25 21:49:55 -0600

    git init
    git add .
    git commit -m "First commit"
    git remote add origin https://github.com/Alexmavl/PortaFolioMarvin.git
    git push -u origin main
    
    git push
    
    initial commit
    git remoto agregar origen https://github.com/Alexmavl/PortafolioMarvin.git
     git rama -M principal
    git push -u origen principal
    git status

--- scripts.js
+++ scripts.js
@@ -1,4 +1,5 @@
 // Obtener el elemento del botón
+<<<<<<< HEAD
 const downloadCVButton = document.getElementById('downloadCV');
 
 // Agregar un listener de eventos al botón
@@ -11,6 +12,20 @@ downloadCVButton.addEventListener('click', () => {
   cvLink.click();
   document.body.removeChild(cvLink);  // Eliminar el enlace del documento
 });
+=======
+  const downloadCVButton = document.getElementById('downloadCV');
+
+        // Agregar un listener de eventos al botón
+        downloadCVButton.addEventListener('click', () => {
+            // Código para descargar el CV
+            const cvLink = document.createElement('a');
+            cvLink.href = 'CV/CV MAVL.pdf';  // Usar barras diagonales
+            cvLink.download = 'CV MAVL.pdf';
+            document.body.appendChild(cvLink);  // Añadir el enlace al documento
+            cvLink.click();
+            document.body.removeChild(cvLink);  // Eliminar el enlace del documento
+        });
+>>>>>>> 3115bf2 (git init)
 
 // Función para enviar el formulario
 document.getElementById('contactForm').addEventListener('submit', function(event) {
@@ -22,7 +37,11 @@ document.getElementById('contactForm').addEventListener('submit', function(event
   const mensaje = document.getElementById('mensaje').value;
 
   // Configura el correo
+<<<<<<< HEAD
   const mailtoLink = `mailto:marvinv708@gmail.com?subject=Contacto de ${nombre}&body=${mensaje}%0D%0A%0D%0AEmail: ${email}`;
+=======
+  const mailtoLink = `mailto:marvin_vsq_94@hotmail.com?subject=Contacto de ${nombre}&body=${mensaje}%0D%0A%0D%0AEmail: ${email}`;
+>>>>>>> 3115bf2 (git init)
 
   // Abre el enlace mailto
   window.location.href = mailtoLink;
